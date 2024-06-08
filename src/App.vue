7d004d9097322ba26b66a32a1ac64e6ba021e86d
hashrock <hashrock@users.noreply.github.com>
2024-06-09 00:26:51 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,144 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <div class="contents">
+      <anydown v-if="mode === 'wysiwyg'" :blocks="splited" @change="updateBlock($event)"></anydown>
+      <textarea class="contents__input" v-if="mode === 'code'" v-model="input" cols="30" rows="10"></textarea>
+    </div>
+    <div class="nav">
+      <label class="nav__item">
+        <input class="nav__item__radio" type="radio" v-model="mode" value="wysiwyg" />
+        <div class="nav__item__label">WYSIWYG</div>
+      </label>
+      <label class="nav__item">
+        <input class="nav__item__radio" type="radio" v-model="mode" value="code" />
+        <div class="nav__item__label">Code</div>
+      </label>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+<script>
+import Anydown from "./components/Anydown.vue";
+import { compile } from "./util/document-compiler";
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+const example = `# anydown
+
+\`\`\`gantt
+New Task,2020-03-31,2020-03-31
+New Task,2020-03-31,2020-03-31
+\`\`\`
+
+\`\`\`kanban
+# backlog
+- test
+
+# todo
+- Hi
+
+# done
+- test
+\`\`\`
+
+\`\`\`block
+item,box,60,40,200,100
+item,box,340,40,200,100
+,arrow-end,260,90,340,90
+,arrow-end,450,270,400,130
+item,box,340,260,200,100
+\`\`\`
+`;
+
+export default {
+  data() {
+    return {
+      input: example,
+      mode: "wysiwyg"
+    };
+  },
+  computed: {
+    splited() {
+      return compile(this.input);
+    }
+  },
+  methods: {
+    updateBlock(payload) {
+      this.splited[payload.id].text = payload.type + "\n" + payload.body;
+      this.input = this.splited.map(i => i.text).join("```");
+    }
+  },
+  watch: {
+    input() {
+      history.replaceState("", "", "?data=" + encodeURIComponent(this.input));
+    }
+  },
+  components: {
+    Anydown
+  },
+  mounted() {
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("data");
+    if (params && data !== null) {
+      this.input = decodeURIComponent(data);
+    }
+  }
+};
+</script>
+<style>
+.wrapper {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.contents {
+  flex: 1;
+  display: flex;
+  overflow-y: scroll;
+  justify-content: center;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.nav {}
+
+.contents__input {
+  background: #333;
+  color: white;
+  flex: 1;
+  padding: 0.5em;
+}
+
+.nav {
+  background: #333;
+  display: flex;
+}
+
+input[type="radio"] {
+  display: none;
+}
+
+input[type="radio"]+.nav__item__label {
+  transition: all 0.2s ease;
+  background: #ccc;
+  padding: 0.5em 1em;
+  box-sizing: border-box;
+  border-top: 0px solid white;
+  cursor: pointer;
+}
+
+input[type="radio"]:checked+.nav__item__label {
+  background: white;
+  border-top: 2px solid rgb(109, 189, 255);
+}
+
+html,
+body {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+.markdown-body {
+  min-width: 800px;
 }
 </style>
---
+++ src/components/MarkdownBlock.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="markdown-block" v-html="compiledMarkdown"></div>
+</template>
+<script>
+import MarkdownIt from "markdown-it";
+const md = new MarkdownIt();
+
+export default {
+  props: {
+    input: String
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return md.render(this.input);
+    }
+  }
+};
+</script>
+<style>
+.markdown-block {
+  min-height: 1rem;
+}
+</style>
